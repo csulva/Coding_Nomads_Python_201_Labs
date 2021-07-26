44445000843e8bc35bdcef71b04cfda14d00e7c8
csulva <chrissulva1@gmail.com>
2021-07-26 16:18:21 -0400

    changes to 03 labs

--- 03_file-input-output/03_01_long_words.py
+++ 03_file-input-output/03_01_long_words.py
@@ -1,2 +1,9 @@
 # Write a program that reads in `words.txt` and prints only the words
 # that have more than 20 characters (not counting whitespace).
+
+with open('words.txt', 'r') as file_in:
+    words = file_in.read()
+    list_of_words = list(words.split())
+    for x in list_of_words:
+        if len(x) > 20:
+            print(x)
--- 03_file-input-output/03_02_words_analysis.py
+++ 03_file-input-output/03_02_words_analysis.py
@@ -1,6 +1,17 @@
+import statistics
+
 # Read in all the words from the `words.txt` file.
 # Then find and print:
 
 # 1. The shortest word (if there is a tie, print all)
 # 2. The longest word (if there is a tie, print all)
 # 3. The total number of words in the file.
+
+with open('words.txt', 'r') as file_in:
+    words = file_in.read()
+    new_list = list(words.split())
+    y = min(new_list, key=len)
+    print(y)
+    z = max(new_list, key=len)
+    print(z)
+    print(len(new_list))
